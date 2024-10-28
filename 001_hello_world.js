060f16fcad94caba78c565f8f3d75a05ef61b0f3
ysk8448 <kehahahaaaa@gmail.com>
2024-10-28 16:01:58 +0900

    폴더이름 변경 및 파일 삭제

--- 001_hello_world.js
+++
@@ -1,8 +0,0 @@
-console.log('Hello World! - js file');
-//서버
-//req요청 고객이 서버로, res응답 서버가 고객에게
-//http header, body
-// req URL(where) How(Method): GET POST PUT DELETE
-/// res code+body
-
-//GET POST
